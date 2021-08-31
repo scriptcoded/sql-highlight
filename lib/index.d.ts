67f1fd450f4e1a4d3fb683c081c735048607f69c
Igor Ordecha <35789870+igoro00@users.noreply.github.com>
2021-08-31 13:50:31 +0200

    move HighlightOptions into module

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,17 +1,17 @@
-export interface HighlightOptions {
-  html?: boolean;
-  classPrefix?: string;
-  colors?: {
-    keyword: string;
-    function: string;
-    number: string;
-    string: string;
-    special: string;
-    bracket: string;
-    clear: string;
-  };
-}
-
 declare module 'sql-highlight' {
+  export interface HighlightOptions {
+    html?: boolean;
+    classPrefix?: string;
+    colors?: {
+      keyword: string;
+      function: string;
+      number: string;
+      string: string;
+      special: string;
+      bracket: string;
+      clear: string;
+    };
+  }
+  
   export function highlight(sqlString: string, options?: HighlightOptions): string
 }
