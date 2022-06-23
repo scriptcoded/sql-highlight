734388899b46a96d2ce61cb4e58757addfc3dd07
erikn69 <erikn_69@hotmail.com>
2022-06-23 14:14:25 -0500

    fix: number regex

--- test/index.js
+++ test/index.js
@@ -14,6 +14,7 @@ console.log(highlight("SELECT COUNT(id), `id`, `username` FROM `users` WHERE `em
 console.log(highlight('SELECT id FROM users'))
 
 console.log(highlight('WITH t1 AS (SELECT data_point FROM tablename) SELECT data_point FROM t1;'))
+console.log(highlight('SELECT f1, f20b, f3a, -1, 1, "1", 1.00 FROM t1;'))
 
 console.log(highlight('SELECT id FROM listings WHERE status = \'not available\''))
 console.log(highlight('SELECT id FROM listings WHERE status = "not available"'))
