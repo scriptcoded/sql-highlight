f75ba74a1442f72eadca2bde9cdff155e3624385
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2023-09-12 10:02:25 +0200

    ci: allow mixed case in commit message headers

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [1, 'always', 100]
+    'body-max-line-length': [1, 'always', 100],
+    'header-case': [1, 'always', 'lower-case']
   }
 }
