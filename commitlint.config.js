569b2b100a344f5e2ca51e0350b38bc693a79372
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2023-09-12 10:52:01 +0200

    ci: allow mixed case in commit message subjects

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [1, 'always', 100],
+    'subject-case': [1, 'always', 'lower-case'],
     'header-case': [1, 'always', 'lower-case']
   }
 }
