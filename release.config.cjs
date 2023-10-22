d5f50337ebdda044a848cdc9a0983f8883af7383
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2023-10-23 00:26:45 +0200

    ci: switch to semantic-release

---
+++ release.config.cjs
@@ -0,0 +1,25 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'master',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true }
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    ['@semantic-release/changelog', {
+      changelogFile: 'CHANGELOG.md'
+    }],
+    ['@semantic-release/git', {
+      assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+      // eslint-disable-next-line no-template-curly-in-string
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }]
+  ]
+}
