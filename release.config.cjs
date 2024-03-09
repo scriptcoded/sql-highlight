4fd4e32aeafa6cec685877d1a3efd7e406893c2a
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2024-03-09 20:54:12 +0100

    ci: add next release channel

--- release.config.cjs
+++ release.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
   branches: [
     '+([0-9])?(.{+([0-9]),x}).x',
     'master',
+    'next',
     { name: 'beta', prerelease: true },
     { name: 'alpha', prerelease: true }
   ],
